1ae18cc60872ac24dd0060a5791b417377ba21f4
Sasarino MARi <tjddlf2645@gmail.com>
2018-10-16 17:47:58 +0900

    First commit

---
+++ Hetzer/Eraser.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Parameters;
+
+namespace Hetzer
+{
+    class Eraser
+    {
+        int counter = 0;
+        readonly int safe_index = 20;
+        long safe_id = 0;
+
+        private Eraser()
+        {
+
+        }
+
+        public static Eraser CreateEraser()
+        {
+            if (Auth.Credentials != null) return new Eraser();
+            else return null;
+        }
+
+        public void StartErase()
+        {
+            while (Delete(GetTimeline())) { }
+        }
+
+        public IEnumerable<Tweetinvi.Models.ITweet> GetTimeline()
+        {
+            var param = CreateParaeater(safe_id);
+            var timeline = User.GetAuthenticatedUser().GetUserTimeline(param);
+            return timeline;
+        }
+
+        public bool Delete(IEnumerable<Tweetinvi.Models.ITweet> timeline)
+        {
+            if (timeline == null || timeline.Count() == 1) return false;
+            foreach (var t in timeline)
+            {
+                if (++counter > safe_index && t.Id != safe_id)
+                {
+                    t.Destroy();
+                }
+                else
+                {
+                    safe_id = t.Id;
+                }
+            }
+            counter--;
+            return true;
+        }
+
+        private UserTimelineParameters CreateParaeater(long safeTweetId)
+        {
+            var p = new UserTimelineParameters
+            {
+                MaximumNumberOfTweetsToRetrieve = 100
+            };
+            if (safeTweetId != 0) p.MaxId = safeTweetId;
+            return p;
+        }
+
+        private void Print(Tweetinvi.Models.ITweet t)
+        {
+            Console.WriteLine(t.CreatedAt.ToString("yyyy-MM-dd hh:mm") + "\t: " + t.Text);
+        }
+    }
+}
---
+++ Hetzer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Tweetinvi;
+using Tweetinvi.Parameters;
+
+namespace Hetzer
+{
+    class Program
+    {
+        const string ct = "";
+        const string cs = "";
+        const string ut = "";
+        const string us = "";
+
+        [STAThread]
+        static void Main(string[] arguments)
+        {
+            Auth.SetUserCredentials(ct, cs, ut, us);
+            while(true)
+            {
+                Thread.Sleep(1000 * 60 * 10);
+                Eraser.CreateEraser().StartErase();
+            }
+
+            //var application = new Application
+            //{
+            //    StartupUri = new Uri("MainWindow.xaml", UriKind.RelativeOrAbsolute)
+            //};
+            //application.Run();
+        }
+    }
+}
